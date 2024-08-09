18c2167adbbe219da125837617751e3546a17994
officialdev2team1 <officialdev2team1@gmail.com>
2024-08-09 18:06:12 +0530

    Made whole webpage on Html and Css

---
+++ js/rotterdam.js
@@ -0,0 +1,25 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+// function myFunction() {
+//     var x = document.getElementById("Navbar");
+//     if (x.className === "nav-part2") {
+//       x.className += " responsive";
+//     } else {
+//       x.className = "nav-part2";
+//     }
+//   }
